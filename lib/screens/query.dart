30300d44ac8ca01b8d1d711783eddad4c0156144
mrCasus <osmanaltuntas@hotmail.com>
2020-05-03 18:41:05 +0300

    xxxx

--- lib/screens/query.dart
+++ lib/screens/query.dart
@@ -1,21 +1,78 @@
+import 'dart:async';
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+
 
 class Query extends StatefulWidget {
   @override
   _QueryState createState() => _QueryState();
 }
 
-class _QueryState extends State<Query> {
-   @override
+class _QueryState extends State<Query>{
+  String barcode = "";
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //key: scaffoldKey,
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {},
-          child: Text("Excel'e Aktar"),
-        ),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Barcode Scanner - googleflutter.com'),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: RaisedButton(
+                    onPressed: barcodeScanning,
+                    child: Text("Capture Image",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    color: Colors.green,
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  margin: EdgeInsets.all(10),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                ),
+                Text("Scanned Barcode Number",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(barcode,
+                  style: TextStyle(fontSize: 25, color:Colors.green),
+                ),
+              ],
+            ),
+          )),
     );
+  }
+
+  //scan barcode asynchronously
+  Future barcodeScanning() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      setState(() => this.barcode = barcode);
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'No camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode =
+      'Nothing captured.');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
   }
 }
